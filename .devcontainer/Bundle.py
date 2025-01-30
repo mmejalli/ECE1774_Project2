7cb4ef60d73008a8e9e8f10be21772005270e048
mmejalli <mim203@pitt.edu>
2025-01-30 08:40:13 -0500

    First Commit

---
+++ .devcontainer/Bundle.py
@@ -0,0 +1,7 @@
+
+class Bundle:
+    def __init__(self, name:str, num_conductors:float, spacing:float, conductor, DSC:float, DSL:float):
+        self.name = name
+        self.num_conductors = num_conductors
+        self.spacing = spacing
+        self.conductor = conductor
---
+++ .devcontainer/Conductor.py
@@ -0,0 +1,9 @@
+
+
+class Conductor:
+    def __init__(self, name:str, diam:float, GMR:float, resistance:float, ampacity:float):
+        self.name = name
+        self.diam = diam
+        self.GMR = GMR
+        self.resistance = resistance
+        self.ampacity = ampacity
